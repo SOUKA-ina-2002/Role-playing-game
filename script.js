9b27d1fb9f910871ec7cace6de5791de80c06a89
ALLALI Soukaina <allalisoukaina2002@gmail.com>
2024-02-21 17:21:52 +0100

    more progress

--- script.js
+++ script.js
@@ -88,10 +88,23 @@ const weapons = [
   }
   
   function buyWeapon() {
+    if(currentWeapon<weapons.length-1){
     if (gold >= 30) {
       gold -= 30;
-      currentWeapon ++;
+      currentWeapon++;
+      goldText.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText = "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
   }
   
   function fightSlime() {
@@ -100,4 +113,11 @@ const weapons = [
   
   function fightBeast() {
   
+  }
+  function sellWeapon() {
+    if (inventory.length > 1) {
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon=inventory.shift();
+    }
   }
