eda6bf836585ae8ee7116f276f50ddc96be64d7a
ALLALI Soukaina <allalisoukaina2002@gmail.com>
2024-02-21 19:11:00 +0100

    almost finished

--- script.js
+++ script.js
@@ -119,5 +119,43 @@ const weapons = [
       gold += 15;
       goldText.innerText = gold;
       let currentWeapon=inventory.shift();
+      text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "Don't sell your only weapon!";
     }
+  }
+  function fightSlime() {
+    fighting = 0;
+    goFight();
+  }
+  
+  function fightBeast() {
+    fighting = 1;
+    goFight();
+  }
+  
+  function fightDragon() {
+    fighting = 2;
+    goFight();
+  }
+  
+  function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
+  }
+  
+  
+  function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power;
+  }
+  
+  function dodge() {
+  
   }
